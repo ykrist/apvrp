1845247a281a3d3cb8e47c67021afdf27dc4f559
yanni_rist <yanni555rist@gmail.com>
2021-03-04 15:49:29 +1000

    finish up task generation

---
+++ src/bin/main.rs
@@ -0,0 +1,32 @@
+use apvrp::*;
+use anyhow::Result;
+
+use instances::dataset::Dataset;
+
+fn main() -> Result<()> {
+  // dbg!(std::env::args());
+  let idx : usize = std::env::args().into_iter().skip(1).next().ok_or(anyhow::Error::msg("Expected integer argument"))?.parse()?;
+  let data = DSET.load_instance(idx)?;
+  let sets = Sets::new(&data);
+
+  let pv_req_t_start : Map<_, _> = data.compat_req_passive.iter()
+    .flat_map(|(&r, pvs)| {
+      let data = &data;
+      let sets = &sets;
+      pvs.iter()
+        .map(move |&p| (
+          (p,r),
+          std::cmp::max(
+            data.start_time[&r],
+            data.travel_time[&(sets.depot, p)] + data.travel_time[&(p, r)] + data.srv_time[&r]
+          )
+        ))
+    })
+    .collect();
+
+  let tasks = Tasks::generate(&data, &sets, &pv_req_t_start);
+
+  println!("{}", tasks.all.len());
+
+  Ok(())
+}
