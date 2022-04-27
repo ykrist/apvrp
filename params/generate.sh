9d4fedc4cf37df532bd23937941814e69f99e473
yanni_rist <yanni555rist@gmail.com>
2022-04-27 10:37:05 +1000

    rename parameters and use 4 cpus

--- params/generate.sh
+++ params/generate.sh
@@ -4,8 +4,10 @@ mkdir -p run
 rm -f run/*
 
 for F in $(ls templates/*) ; do
+    I=0
     for S in $(cat seeds.txt) ; do
-        NAME="$(jq -r .param_name $F)_$S"
+        NAME="$(jq -r .param_name $F)_$I"
+        I=$(( I + 1 ))
         echo $NAME
         DEST=run/${NAME}.json
         jq ".gurobi += [[\"Seed\", {\"Int\": $S}]] | .param_name=\"$NAME\"" $F > run/${NAME}.json
